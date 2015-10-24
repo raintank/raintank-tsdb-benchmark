c23301bfa75788942993c2d9d566b58647c7d3c3
Dieter Plaetinck <dieter@raintank.io>
2015-10-24 16:24:52 -0400

    convenience++

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
-helps to easily stresstest a TSDB backend.
+stresstest and measure performance of the TSDB backend of a raintank stack.
 
 targets specifically metrics from env-load within a standard devstack with 1 collector.
 make sure you have a good open file handles limit. see https://rtcamp.com/tutorials/linux/increase-open-files-limit/
 
-1. install https://github.com/tsenart/vegeta
-2. run standard dev-stack with one collector
-3. run env-load. it defaults to 100 orgs
-4. wait until all metrics exist (see graphite-watcher and sys dashboard)
+# requirements
+
+* https://github.com/tsenart/vegeta
+* [raintank-docker](https://github.com/raintank/raintank-docker) aka devstack
+* [graphite-watcher](https://github.com/raintank/raintank-metric/tree/tank/graphite-watcher)
+* [env-load](https://github.com/raintank/env-load)
+
+# how to
+
+1. in devstack in grafana-dev/conf/custom.ini: alerting enabled false.
+   alerting stresses grafana cpu too much, also sometimes the system can't keep up and it's hard to include that data in the results.
+2. ./setup_dev.sh
+3. ./launch_dev.sh
+    runs standard dev-stack with one collector
+4. verify measure, graphite-watcher, grafana are running in screen. login to grafana and check that sys dashboard works.
 5. ./run.sh 100
-6. check results directory
+6. when it completes, the results are only valid if:
+   * run.sh/vegeta didn't show any errors
+   * graphite-watcher didn't error
+   * graphite-api and cassandra didn't error
+   * your host hard drive didn't run full, that tends to cause slowdowns
+7. your data is in the results directory and in the grafana dashboards, take a snapshot.
+
+# watch out for:
+## high cpu due to wireless drivers
+if i'm on wifi, even though everything should go through loopbacks,
+a lot of cpu time is spent in iwlwifi kernel process, and in grafana/nsq_* processes that do a lot of network io (for redis, statsd, etc).
+(can easily be confirmed with profiling). the solution is just disable wifi on the host
--- run.sh
+++ run.sh
@@ -7,14 +7,15 @@ if [ -z "$orgs" ]; then
   exit 2
 fi
 
-fulllist=$(mktemp)
-for org in $(seq 1 $orgs); do
-  for endp in {1..4}; do
-    cat env-load-metrics-patterns.txt | sed -e "s#\$org#$org#" -e "s#\$endp#$endp#" >> $fulllist
+function writePatterns () {
+  fulllist=$(mktemp)
+  for org in $(seq 1 $orgs); do
+    for endp in {1..4}; do
+      cat env-load-metrics-patterns.txt | sed -e "s#\$org#$org#" -e "s#\$endp#$endp#" >> $fulllist
+    done
   done
-done
-
-echo "list is at $fulllist -- it is $(wc -l $fulllist) lines long"
+  echo "metric patterns are at $fulllist -- it is $(cat $fulllist | wc -l) lines long"
+}
 
 function postEvent() {
   curl -X POST "localhost:8086/db/raintank/series?u=graphite&p=graphite" -d '[{"name": "events","columns": ["type","tags","text"],"points": [['"\"$1\", \"$2\",\"$3\"]]}]"
@@ -26,11 +27,11 @@ function runTest () {
   local duration=$3
   f="results/$key-$range-$duration"
 
-  waitTimeBoundary
+  waitTimeBoundary 1
 
   echo "################## $(date): $1, $range time range. test duration: $duration START ###################"
   postEvent "bench-start" "" "benchmark $1, $range time range. test duration: $duration"
-  sed "s#^#GET http://localhost:8888/render?target=\&from=-$span=#" | vegeta attack -duration 60s -rate 2000 > $f.bin
+  sed "s#^#GET http://localhost:8888/render?target=\&from=-$span=#" | vegeta attack -duration 60s -rate 200 > $f.bin
   cat $f.bin | vegeta report
   cat $f.bin | vegeta report -reporter="hist[0,100ms,200ms,300ms]"
   cat $f.bin | vegeta report -reporter=plot > $f.html
@@ -38,15 +39,30 @@ function runTest () {
   postEvent "bench-stop" "" "benchmark $1, $range time range. test duration: $duration"
 }
 
+# for an arg of e.g. 10 minutes,
 # waits until the clock is a nice round number, divisible by 10 minutes, at least 10 or more minutes in the future.
+# so between 10 and 20 minutes from now.
 function waitTimeBoundary() {
+  local minutes=$1
+  local seconds=$((minutes * 60))
+  local twiceMinutes=$(($minutes * 2))
   now=$(date +%s)
-  #nextMark=$(( $(( $(date -d '+ 20 minutes' +%s) / 600)) * 600))
-  nextMark=$(( $(( $(date -d '+ 2 minutes' +%s) / 60)) * 60))
+  nextMark=$(( $(( $(date -d "+ $twiceMinutes minutes" +%s) / $seconds)) * $seconds))
   diff=$(($nextMark - $now))
-  echo waiting $diff seconds for next test...
+  echo waiting $diff seconds
   sleep $diff
 }
+
+waitTimeBoundary 1
+postEvent "env-load start" "" "env-load loading $orgs orgs"
+env-load -orgs $orgs load
+postEvent "env-load finished" "" "env-load loaded $orgs orgs"
+writePatterns
+
+echo "please wait for $orgs (orgs) * 4 (endpoints per org) * 30 = $(($orgs * 4 * 30)) metrics to show up in ES (see sys dashboard)"
+echo "press a key to proceed when ready"
+read
+echo "continuing..."
 
 head -n 1 $fulllist | runTest "min-diversity" 5min 60s
 cat $fulllist | runTest "max-diversity" 5min 60s
